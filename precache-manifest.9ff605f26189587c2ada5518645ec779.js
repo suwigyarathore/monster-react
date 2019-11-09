513d4c45ebda691ec6ba33220ff1924955c882bf
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-11-10 00:48:47 +0100

    Updates

---
+++ precache-manifest.9ff605f26189587c2ada5518645ec779.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58290978167f1eaaeb96d1ea81b7a979",
+    "url": "/monster-react/index.html"
+  },
+  {
+    "revision": "16fd2aa450e5fad9a1bd",
+    "url": "/monster-react/static/css/main.a0570808.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monster-react/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "16fd2aa450e5fad9a1bd",
+    "url": "/monster-react/static/js/main.84017513.chunk.js"
+  },
+  {
+    "revision": "2d2f7a5b984dfef60e80",
+    "url": "/monster-react/static/js/runtime-main.2e540fba.js"
+  }
+]);
